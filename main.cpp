d8ccd6ac7ca05c1539ae32d8e77a0af196992f9e
Donytriprasetyo <45379261+Donytriprasetyo@users.noreply.github.com>
2020-07-14 15:03:24 +0700

    Source code utama
    source code utama

---
+++ main.cpp
@@ -0,0 +1,154 @@
+#include<windows.h>
+#include<gl/glut.h>
+
+float xrot = 0.0f;
+float yrot = 0.0f;
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+int z1 = 0;
+int x1 = 0;
+int y1 = 0;
+bool mouseDown = false;
+int is_depth;
+
+void bebek();
+
+void init(void){
+    glClearColor(0, 1, 254/255.f, 0);
+    glMatrixMode(GL_PROJECTION);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+    is_depth=1;
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void drawCylinder(float rbase,float rtop,float height){
+    float i;
+    glPushMatrix();
+    glTranslatef(0,0.0,-rbase/4);
+    for(i=0;i<=height;i+=rbase/25)
+    {
+        glTranslatef(0,0.0,rbase/25);
+        glutSolidTorus(3,rbase-((i*(rbase-rtop))/height),20,16);
+    }
+    glTranslatef(0,0.0,rbase/4);
+    glPopMatrix();
+}
+
+void mouse(int button, int state, int x, int y){
+    if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+        mouseDown = true;
+        xdiff = x - yrot;
+        ydiff = -y + xrot;
+    }
+    else
+        mouseDown = false;
+}
+
+void mouseMotion(int x, int y){
+    if(mouseDown){
+        yrot = x - xdiff;
+        xrot = y + ydiff;
+
+        glutPostRedisplay();
+    }
+    glLoadIdentity();
+    gluLookAt(0.0f, 0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+    glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+}
+
+void idle(){
+    if(!mouseDown){
+        xrot += 0.0000005f;
+        yrot += 0.0000005f;
+    }
+    glutPostRedisplay();
+}
+
+void keyboard(unsigned char key,int x, int y)
+{
+    switch(key)
+    {
+
+    case 'w':
+    case 'W':
+        glTranslatef(0.0, 0.0, 3.0);
+        break;
+    case 'd':
+    case 'D':
+        glTranslatef(3.0, 0.0, 0.0);
+        break;
+    case 's':
+    case 'S':
+        glTranslatef(0.0, 0.0, -3.0);
+        break;
+    case 'a':
+    case 'A':
+        glTranslatef(-3.0, 0.0, 0.0);
+        break;
+    case '7':
+        glTranslatef(0.0, 3.0, 0.0);
+        break;
+    case '9':
+        glTranslatef(0.0, -3.0, 0.0);
+        break;
+    case '2':
+        glRotatef(2.0, 1.0, 0.0, 0.0);
+        break;
+    case '8':
+        glRotatef(-2.0, 1.0, 0.0, 0.0);
+        break;
+    case '6':
+        glRotatef(2.0, 0.0, 1.0, 0.0);
+        break;
+    case '4':
+        glRotatef(-2.0, 0.0, 1.0, 0.0);
+        break;
+    case '1':
+        glRotatef(2.0, 0.0, 0.0, 1.0);
+        break;
+    case '3':
+        glRotatef(-2.0, 0.0, 0.0, 1.0);
+        break;
+    }
+
+}
+
+void ukuran(int lebar, int tinggi){
+    if(tinggi == 0) tinggi = 1;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(50, lebar / tinggi, 1, 500);
+    glTranslatef(0, 0, -150);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void display(void){
+    if(is_depth){
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    } else {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("PROJECT_TR_GRAFKOM");
+    glutDisplayFunc(display);
+
+    glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
+    glutMotionFunc(mouseMotion);
+    glutReshapeFunc(ukuran);
+    init();
+
+    glutIdleFunc(display);
+    glutMainLoop();
+    return 0;
+}
